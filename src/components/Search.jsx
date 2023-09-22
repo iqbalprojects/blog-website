3f5f997e360c7fee0b754a862b2b4f366ceebece
Iqbal <iqbaljacksonsite@gmail.com>
2023-09-22 16:15:09 +0700

    improvement page speed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,11 +5,11 @@ const Search = () => {
     return (
         <div>
             <form action="">
-                <label htmlFor="search" className="relative">
+                <label htmlFor="searchBar" className="relative">
                     <Magnify className="absolute right-3 bottom-0" />
                     <input
                         type="text"
-                        name="search"
+                        name="searchBar"
                         id=""
                         placeholder="Search..."
                         className="bg-[#F5F5F5] px-3 h-10 rounded-lg text-black"
